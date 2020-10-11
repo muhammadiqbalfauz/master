c63e5c2bc6627b871c6bbdf09e58f66b4382f92b
muhammadiqbalfauz <72346102+muhammadiqbalfauz@users.noreply.github.com>
2020-10-11 17:31:47 +0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+class Hero: 
+    pass
+
+
+hero1 = Hero()
+hero2 = Hero()
+
+hero1.name = "sniper"
+hero1.healt = 100
+
+hero2.name = "sven"
+hero2.healt = 200
+
+print(hero1)
+print(hero1.__dict__)
+print(hero2.name)
